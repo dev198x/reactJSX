150e743571b3fee9b5b1e8da0bc3274b5312d079
dev198x <138682521+dev198x@users.noreply.github.com>
2023-10-05 08:55:01 +0700

    Initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Profile() {
+  // my comment
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Profile from "./components/Profile";
+
+ReactDOM.render(
+  <>
+    <h1>Hello World</h1>
+    <p>This is a paragraph</p>
+    {/* my comment your code */}
+    <Profile />
+  </>,
+  document.getElementById("root")
+);
